70878989ce6fa77ee4eb5c12d0cdf4dc80a21bcf
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-27 11:03:19 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, accuracy_score
 
 healthcare_filepath = "data/healthcare_dataset.csv"
 data = pd.read_csv(healthcare_filepath)
@@ -33,3 +36,17 @@ avg_billing = data.groupby(age_groups)['Billing Amount'].mean()
 avg_billing.plot(kind='bar')
 plt.ylabel('Average Billing Amount ($)')
 plt.savefig('billing_by_age.png')
+
+# Target: Test Results
+
+X = data.drop(['Test Results'], axis=1)
+y = data['Test Results']
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+model.fit(X_train, y_train)
+preds = model.predict(X_valid)
+
+print('MAE:', mean_absolute_error(y_valid, preds))
+print('Accuracy:', accuracy_score(y_valid, preds))
+
